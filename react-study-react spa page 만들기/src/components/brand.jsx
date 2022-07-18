9b931d3fe7a184f331a8e52931eef092a43c63ee
huunguk <huunguk@gmail.com>
2022-07-18 12:52:25 +0900

    spa homepage 만들기

---
+++ react-study-react spa page 만들기/src/components/brand.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./footer";
+
+
+function Brand() {
+  return (
+    <>
+      <div>
+        Brand
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Brand;
---
+++ react-study-react spa page 만들기/src/components/coupon.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./footer";
+
+
+function Coupon() {
+  return (
+    <>
+      <div>
+        Conpon
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Coupon;
